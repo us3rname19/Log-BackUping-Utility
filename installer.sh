78ad80ddf62ea96b90aac7189b1e546c6fb171b7
us3rname19 <cod3r@programmer.net>
2022-03-30 14:39:43 +1000

    fix installer

--- installer.sh
+++ installer.sh
@@ -6,21 +6,14 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
-
-if [[ -n $(cat /etc/os-release |grep debian) ]]
-then
+if [[ -n $(cat /etc/os-release |grep debian) ]]; then
 	#sudo apt install -y 
+elif [[ -n $(cat /etc/os-release |grep fedora) ]]; then
+    #sudo yum install -y 
 fi
-
-if [[ -n $(cat /etc/os-release |grep fedora) ]]
-
-then
-	#sudo yum install -y 
-fi
-
 
 # this will automatically install the required package
 sudo cp lbckp_src/etc/init.d/lbckp /etc/init.d
-sudo cp -R lbckp_src/usr/bin/ /usr/bin 
+sudo cp -R lbckp_src/usr/bin/* /usr/bin
 
 exit 0
